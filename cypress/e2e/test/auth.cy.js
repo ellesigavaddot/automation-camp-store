1e4c8b9b87b8866a8c87925275380040434803d8
Giselle T <giselleatodd@gmail.com>
2022-12-09 18:26:21 -0500

    Login authentication

---
+++ cypress/e2e/test/auth.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+import AuthPage from '../page/auth.page'
+
+describe('Login and Sign Up', ()=> {
+
+    before(()=>{
+        cy.visit('/')
+        cy.get(AuthPage.buttonSigninRegister).click()
+
+    })
+
+    it('should login an existing user', ()=> {
+        cy.origin(
+            "https://dev-mlluudmotpwoldtv.us.auth0.com/login",
+            { args: {} },
+            ({}) => {
+            cy.get('[type="email"]').type("test123@gmail.com");
+            cy.get('[type="password"]').type("Password1!", { log: false });
+            cy.get("[name=submit]").click();
+        })
+
+    })
+
+})
