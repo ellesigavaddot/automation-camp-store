8029f69f8eb9c81275a5896b93b14567299ede7a
Giselle T <giselleatodd@gmail.com>
2022-12-14 07:46:14 -0500

    Create checkout page and checkout test file

---
+++ cypress/e2e/page/checkout.page.js
@@ -0,0 +1,26 @@
+
+class CheckoutPage{
+
+//#region Getters
+get subheaderBilling(){ return ('h1.snipcart__font--subtitle')}
+
+get inputFullname(){return ('input#name_8c0ebc01-a93f-491a-b439-b2c40a319df2') }
+
+get inputEmail(){ return ('input#email_8c0ebc01-a93f-491a-b439-b2c40a319df2')}
+
+get inputStrAdd(){ return ('')}
+
+get inputApt(){ return ('input#address2_8c0ebc01-a93f-491a-b439-b2c40a319df2')}
+
+get inputCity(){ return ('input#address2_8c0ebc01-a93f-491a-b439-b2c40a319df2')}
+
+get buttonContinuePay(){ return ('')}
+//#endregion
+
+
+//#region Methods
+//#endregion
+
+
+}
+export default new CheckoutPage()
