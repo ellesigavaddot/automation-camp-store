7fe6839e4910e729d94f1a30715d3ed042bc8540
Giselle T <giselleatodd@gmail.com>
2022-12-16 11:12:12 -0500

    Update product gallery test

--- cypress/e2e/test/productHomepage.cy.js
+++ cypress/e2e/test/productHomepage.cy.js
@@ -23,9 +23,11 @@ describe('Product Gallery Homepage', ()=>{
 
     it('should highlight nav item for current page', ()=>{
         cy.visit('/contact')
+        //navigate to contact and ensure it is highlighted in the nav
         cy.get('button').contains('Contact').should('have.class', 'css-betff9')
         cy.get('button').contains('Home').should('not.have.class', 'css-betff9')
 
+        //navigate to home and check that Home menu item is in focus/highlighted
         cy.get('button').contains('Home').click()
         cy.get('button').contains('Contact').should('not.have.class', 'css-betff9')
 
@@ -33,8 +35,8 @@ describe('Product Gallery Homepage', ()=>{
 
     it('should verify product has second image', ()=>{
         cy.visit('/products')
-        Homepage.clickImage(0)
-        cy.url().should('eq','https://ui-automation-camp.vercel.app/products/quality-hat-model')
+        Homepage.clickImage(0) //click the image of the first product
+        cy.url().should('eq','https://ui-automation-camp.vercel.app/products/quality-hat-model') //assert on the product url
 
         cy.get(Homepage.selectedImageLegend).should('have.text', 'Image 1')
         Homepage.toSecondImg()
