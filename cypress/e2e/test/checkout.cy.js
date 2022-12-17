106fd4b76d1a612ffc2c0200b2aac45585c9546f
Giselle T <giselleatodd@gmail.com>
2022-12-17 06:29:34 -0500

    Add describe and first test to chechkout file

---
+++ cypress/e2e/test/checkout.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+import ProductPage from '../page/product.page'
+import CheckoutPage from '../page/checkout.page'
+import OrderConfirmationPage from '../page/orderConf.page'
+
+
+describe('Checkout from Cart', ()=>{
+    before(()=>{
+
+
+    })
+
+
+    it('should checkout single item from cart', ()=>{
+        
+
+
+
+    })
+
+
+
+
+
+
+
+})
